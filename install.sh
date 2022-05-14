877e24b029bb5e7b44766b79684b89eb1c14922c
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-05-14 16:49:37 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+echo "Installing Libs for Bluetooth"
+
+sudo apt install pip
+
+sudo apt-get install bluetooth bluez libbluetooth-dev
+sudo python3 -m pip install pybluez
+
+mv pystart.py ..
+
+echo "Installing OpenCV Python Libraries"
+
+sudo apt install pip
+
+sudo apt install libglew-dev
+
+sudo pip3 install opencv-contrib-python
+
+cd ..
+mv rpi-cv rpi
+mv rpi-bt/* rpi
+sudo rm -R rpi-bt
+
+echo "python3 ~/pystart.py &" >> ~/.bashrc
+
+systemctl stop serial-getty@ttyS0.service
+
+echo "Remove the first two chunks from '/boot/cmdline.txt' to make communication work!"
