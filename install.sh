148d649c807ad60799322b3bb905f4e862a7d256
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-05-16 16:44:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 echo "Installing Libs for Bluetooth"
 
+sudo apt update
 sudo apt install pip
-sudo apt-get install bluetooth bluez libbluetooth-dev -y
-sudo python3 -m pip install pybluez
+sudo apt install python3-gpiozero -y
+sudo apt install bluetooth bluez libbluetooth-dev -y
 sudo apt install libglew-dev -y
+sudo python3 -m pip install pybluez
+sudo pip3 install gpiozero
 
 mv pystart.py ..
 
-sudo pip3 install opencv-contrib-py
+sudo pip3 install opencv-contrib-python
 
 sudo echo "python3 ~/pystart.py &" >> ~/.bashrc
 
 sudo systemctl stop serial-getty@ttyS0.service
 
-echo "Edit '/boot/cmdline.txt' AND Enable Camera and Serial"
+echo "disable serial0 - 'sudo nano /boot/cmdline.txt' [AND] enable camera and serial communication - 'sudo raspi-config'"
