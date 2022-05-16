cae738ca3e35c9af02b6ee1881e9457ab5995e46
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-05-16 17:24:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,11 @@ sudo apt install pip
 sudo apt install python3-gpiozero -y
 sudo apt install bluetooth bluez libbluetooth-dev -y
 sudo apt install libglew-dev -y
-sudo python3 -m pip install pybluez
-sudo pip3 install gpiozero
-
-mv pystart.py ..
 
 sudo pip3 install opencv-contrib-python
+sudo python3 -m pip install pybluez
+
+mv pystart.py ..
 
 sudo echo "python3 ~/pystart.py &" >> ~/.bashrc
 
